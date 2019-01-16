cd46b05a2577795791a59c3f15ce569d5df147e9
Nat Sharpe <sharpe1890@gmail.com>
2019-01-16 15:26:41 -0500

    Returns message containing three input answers

--- nodeFile.js
+++ nodeFile.js
@@ -1,18 +1,33 @@
 const questions = [
   "What is your name?",
-  "What is your fav movie?",
-  "What is your lovedest coding language?",
+  "What is your fav hobby?",
+  "What is your favorite coding language?",
 ];
 
 let answers = [];
 
-let ask = (index) => {
+const ask = (index) => {
   process.stdout.write(`\n\n\n\n ${questions[index]}`);
   process.stdout.write(" > ");
 };
 
 process.stdin.on('data', (data) => {
-  process.stdout.write('\n' + data.toString().trim() + '\n');
+
+  answers.push(data.toString().trim());
+
+  answers.length < questions.length ? 
+    ask(answers.length) : process.exit();
+
 })
+
+process.on('exit', () => {
+
+  process.stdout.write('\n\n\n\n');
+
+  process.stdout.write(`Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} tomorrow!`)
+
+  process.stdout.write('\n\n\n\n');
+
+});
 
 ask(0);
